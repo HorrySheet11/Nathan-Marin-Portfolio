7acef90167a71dddee483eb3af017196a1463db4
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2026-02-28 12:34:03 +0800

    added some style for Sidebar
    working on toggle animation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,33 @@
+import pfp from "../assets/author-image.jpg"
+import { useState } from "react";
+
 function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const nav = {
+    "About me": "#about-me",
+    Projects: "#projects",
+    Skills: "#skills",
+    Contact: "#contact",
+  }
+
   return (
-    <div id="sidebar-wrapper" class='bg-blue-50 p-10'>
-      <ul class="sidebar-nav">
-        <li class="sidebar-brand">
-          <a href="#sample" class="bg-red">Nathan Marin</a>
-        </li>
-        <li>
-          <a href="#section1">About Me</a>
-        </li>
-        <li>
-          <a href="#section2">Projects</a>
-        </li>
-        <li>
-          <a href="#section3">Contact</a>
-        </li>
-      </ul>
+    <div id="sidebar-wrapper" className='bg-linear-to-t from-blue-700 to-cyan-500 p-10 font-bold text-2xl h-screen w-max fixed left-0 transition ease-in-out duration-300'>
+      <button type="button" className="absolute top-5 -right-5" onClick={() => setIsOpen(!isOpen)} aria-controls="sidebar-wrapper">toggle</button>
+      <div className="inline-block cursor-pointer " onclick={() => setIsOpen(!isOpen)}>
+        <div className="w-35 h-5 bg-white mt-6 mb-6 mr-0 ml-0 transition duration-400"></div>
+        <div className="w-35 h-5 bg-white mt-6 mb-6 mr-0 ml-0 transition duration-400"></div>
+        <div className="w-35 h-5 bg-white mt-6 mb-6 mr-0 ml-0 transition duration-400"></div>
+      </div>
+      <nav className="h-full">
+        <ul className="sidebar-nav [&>*]:m-3 h-full flex flex-col items-center justify-center">
+          <img src={pfp} alt="Nathaniel Marin" class="rounded-full h-50 w-50" />
+          {Object.entries(nav).map(([key, value]) => (
+            <li key={key} class="hover:scale-110 transition ease-in-out duration-200 cursor-pointer">
+              <a href={value}>{key}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   )
 }
