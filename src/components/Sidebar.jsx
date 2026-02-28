112483f0a068b2c32fcac2069c44d9bc22849db7
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2026-02-28 20:47:38 +0800

    added transition for sidebar button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import pfp from "../assets/author-image.jpg"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Sidebar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,14 +10,22 @@ function Sidebar() {
     Contact: "#contact",
   }
 
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("open")
+    } else {
+      document.body.classList.remove("open")
+    }
+  }, [isOpen])
+
   return (
     <div id="sidebar-wrapper" className='bg-linear-to-t from-blue-700 to-cyan-500 p-10 font-bold text-2xl h-screen w-max fixed left-0 transition ease-in-out duration-300'>
-      <button type="button" className="absolute top-5 -right-5" onClick={() => setIsOpen(!isOpen)} aria-controls="sidebar-wrapper">toggle</button>
-      <div className="inline-block cursor-pointer " onclick={() => setIsOpen(!isOpen)}>
-        <div className="w-35 h-5 bg-white mt-6 mb-6 mr-0 ml-0 transition duration-400"></div>
-        <div className="w-35 h-5 bg-white mt-6 mb-6 mr-0 ml-0 transition duration-400"></div>
-        <div className="w-35 h-5 bg-white mt-6 mb-6 mr-0 ml-0 transition duration-400"></div>
-      </div>
+      {/* <button type="button" className="absolute top-5 -right-5" onClick={() => setIsOpen(!isOpen)} aria-controls="sidebar-wrapper">toggle</button> */}
+      <button type="button" className="inline-block cursor-pointer " onClick={() => setIsOpen(!isOpen)}>
+        <div className={`w-10 h-1.5 bg-white mt-1.5 mb-1.5 mr-0 ml-0 rounded-xs transition duration-400 ${isOpen && "translate-x-0 translate-y-[12px] -rotate-45"}`}></div>
+        <div className={`w-10 h-1.5 bg-white mt-1.5 mb-1.5 mr-0 ml-0 rounded-xs transition duration-400 ${isOpen && "opacity-0"}`}></div>
+        <div className={`w-10 h-1.5 bg-white mt-1.5 mb-1.5 mr-0 ml-0 rounded-xs transition duration-400 ${isOpen && "translate-x-0 translate-y-[-12px] rotate-45"}`}></div>
+      </button>
       <nav className="h-full">
         <ul className="sidebar-nav [&>*]:m-3 h-full flex flex-col items-center justify-center">
           <img src={pfp} alt="Nathaniel Marin" class="rounded-full h-50 w-50" />
