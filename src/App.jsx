8bf81bba05ae4f208a309dbf3cd413d17d08b1b7
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2026-03-03 11:23:22 +0800

    added scroll progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import Projects from "./pages/Projects.jsx";
 
 function App() {
 	return (
-		<>
+		<div className="relative">
 			<Sidebar />
+			<div id="el" className=" h-5 w-full bg-linear-to-r from-blue-700 to-cyan-500 fixed -left-1/2 bottom-0  rounded-r-2xl"></div>
 			<AboutMe />
 			<Bar />
 			<Projects />
-		</>
+		</div>
 	);
 }
 
