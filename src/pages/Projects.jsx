6da30085a2b3162f57a6026dce385ec9d7074497
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2026-03-04 20:09:41 +0800

    fixed project div sizing
    added a few transitions

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,51 +1,58 @@
-import P1 from '../assets/samplePic.png'
+import P1 from "../assets/samplePic.png";
 
 function Projects() {
 	const projects = {
 		"Project 1": {
-			link: "sampleLink.com",
+			link: "sampleLink1.com",
 			description: "lorem ipsum dolor sit amet",
 			image: P1,
 			alt: "lorem ipsum",
 		},
 		"Project 2": {
-			link: "sampleLink.com",
+			link: "sampleLink2.com",
 			description: "lorem ipsum dolor sit amet",
 			image: P1,
 			alt: "lorem ipsum",
 		},
 		"Project 3": {
-			link: "sampleLink.com",
+			link: "sampleLink3.com",
 			description: "lorem ipsum dolor sit amet",
 			image: P1,
 			alt: "lorem ipsum",
 		},
 		"Project 4": {
-			link: "sampleLink.com",
+			link: "sampleLink4.com",
 			description: "lorem ipsum dolor sit amet",
 			image: P1,
 			alt: "lorem ipsum",
 		},
-		
 	};
 
-	const hiddenHover = "desc transition-all ease-in-out duration-300 opacity-0 h-0 group-hover:h-auto group-hover:opacity-100"
+	const hiddenHover =
+		"desc transition-all ease-in-out duration-300 opacity-0 h-0 group-hover:h-auto group-hover:opacity-100";
 	return (
 		<section
 			key="projects"
 			id="projects"
-			className="p-52 section w-full h-auto flex flex-col items-center justify-center gap-30 "
+			className="section h-auto flex flex-col items-center justify-center gap-30 "
 		>
-			<h1>Projects</h1>
-			<div className="flex flex-row gap-5	h-full w-full">
+			<h1 className="font-bold">Projects</h1>
+			<div className="flex flex-row items-center justify-center	h-full w-full flex-wrap">
 				{Object.entries(projects).map(([key, value]) => (
-					<a key={key} href={value.link} className="group w-100 hover:scale-105 transition ease-in-out duration-300	">
-						<div className="w-100% bg-gray-700 rounded-2xl p-3 *:m-2 place-content-center flex flex-col">
-							<img src={value.image} alt={value.alt} className="h-100% width-auto rounded-xl block" />
+					<div key={key} className="max-w-fit bg-gray-700 rounded-2xl p-3 *:m-2 place-content-center flex flex-col m-5 transition-all ease-in-out duration-300 hover:max-h-auto hover:scale-105 hover:bg-gray-800">
+						<a
+							href={value.link}
+							className="group w-150 transition ease-in-out duration-300	"
+						>
+							<img
+								src={value.image}
+								alt={value.alt}
+								className="h-100% width-auto rounded-xl block"
+							/>
 							<h4 className={hiddenHover}>{key}</h4>
 							<p className={hiddenHover}>{value.description}</p>
-						</div>
-					</a>
+						</a>
+					</div>
 				))}
 			</div>
 		</section>
