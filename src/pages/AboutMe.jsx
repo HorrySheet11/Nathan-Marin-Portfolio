c5e6afef154b97affd255c90c2c3edee87beeaa0
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2026-06-06 19:54:31 +0800

    feat: added about me small breakpoint

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,13 +4,13 @@ function AboutMe() {
 	return (
 		<section
 			id="about-me"
-			className="section h-screen grid place-items-center grid-cols-2 gap-10"
+			className="section grid place-items-center grid-cols-2 gap-10 xs:max-sm:gap-3"
 		>
-			<img src={pfp} alt="about-me-pic" className="rounded-full w-100" />
-			<div className="*:mb-5">
+			<img src={pfp} alt="about-me-pic" className="rounded-full w-100 xs:max-sm:self-start" />
+			<div className="*:mb-5 xs:max-sm:**:mb-2 xs:max-sm:self-start">
 				<h1 className="font-bold">Nathaniel Marin</h1>
-				<h5 className="">Aspiring Web Developer</h5>
-				<p className="indent-3">
+				<h5 className="xs:max-sm:text-sm">Aspiring Web Developer</h5>
+				<p className="indent-3 xs:max-sm:text-xs">
 					I am a person who is willing to be enlightened and willing to learn
 					more as a successful programmer. I currently finished my course in
 					Bachelor of Science in Computer Science and aspire to be a successful
