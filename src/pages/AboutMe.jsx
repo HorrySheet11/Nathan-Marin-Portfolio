a7523a10df8d71deb5a2df21f7d06a71bb46809d
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2026-06-09 11:48:47 +0800

    feat: finished about-me page proportions from md to xl

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,15 +4,23 @@ function AboutMe() {
 	return (
 		<section
 			id="about-me"
-			className="section grid place-items-center grid-cols-2 gap-10 small-section xs:max-sm:gap-3 xs:max-md:grid-cols-1 
-			sm:max-md:p-13 sm:max-md:gap-5"
+			className="section grid place-items-center grid-cols-2 gap-10 small-section 
+			xs:max-sm:gap-3 xs:max-md:grid-cols-1 
+			sm:max-md:p-13 sm:max-md:gap-5 
+			md:max-lg:p-13 md:max-lg:grid-cols-1 
+			lg:max-xl:p-40 lg:max-xl:gap-5 
+			xl:max-2xl:p-13 xl:max-2xl:gap-5"
 		>
 			<img
 				src={pfp}
 				alt="about-me-pic"
-				className="rounded-full w-100 xs:max-sm:self-start xs:max-md:w-40"
+				className="rounded-full w-100 xs:max-sm:self-start 
+				xs:max-md:w-40 
+				md:max-lg:w-60
+				lg:max-xl:w-80
+				xl:max-2xl:w-100"
 			/>
-			<div className="*:mb-5 xs:max-sm:**:mb-2 sm:max-md:**:mb-3">
+			<div className="*:mb-5 xs:max-sm:**:mb-2 sm:max-md:**:mb-3 ">
 				<h1 className="font-bold ">Nathaniel Marin</h1>
 				<h5 className="">Aspiring Web Developer</h5>
 				<p className="indent-3">
